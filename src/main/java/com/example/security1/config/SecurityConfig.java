6080b8e6589fe2a5270f3470f04658618633c98c
asst2307024 <asst2307024@tosspartners.com>
2023-07-09 22:52:25 +0900

    004 - 시큐리티 로그인

--- src/main/java/com/example/security1/config/SecurityConfig.java
+++ src/main/java/com/example/security1/config/SecurityConfig.java
@@ -3,13 +3,10 @@ package com.example.security1.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
-
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -17,13 +14,25 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
 
     @Bean
+    public BCryptPasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+        http.csrf(csrf -> csrf.disable());
         http
+                .cors(cors -> cors.disable())
                 .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/manager/**").permitAll()
+                        .requestMatchers("/user/**").hasAnyRole("USER_ADMIN", "USER_MANAGER")
                         .anyRequest().authenticated()
                 )
+                .formLogin((formLogin) ->
+                        formLogin
+                                .loginPage("/loginForm")
+                                .loginProcessingUrl("/login")
+                                .defaultSuccessUrl("/"))
                 .httpBasic(withDefaults());
         return http.build();
     }
--- src/main/java/com/example/security1/controller/IndexController.java
+++ src/main/java/com/example/security1/controller/IndexController.java
@@ -1,11 +1,22 @@
 package com.example.security1.controller;
 
+import com.example.security1.model.User;
+import com.example.security1.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @GetMapping({"", "/"})
     public String index(){
@@ -31,20 +42,25 @@ public class IndexController {
     }
 
     @GetMapping("/login")
-    @ResponseBody
     public String login(){
-        return "login";
+        return "loginForm";
     }
 
     @GetMapping("/join")
-    @ResponseBody
-    public String join(){
+    public String join() {
         return "join";
     }
 
-    @GetMapping("/joinProc")
-    public @ResponseBody String joinProc(){
-        return "회원가입 완료됨!";
+    @PostMapping("/joinProc")
+    @GetMapping
+    public String joinProc(User user) {
+        System.out.println("회원가입 진행 : " + user);
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        user.setRole("ROLE_USER");
+        userRepository.save(user);
+        return "redirect:/";
     }
 
 }
